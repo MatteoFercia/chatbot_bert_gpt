e43fabcc9bf41b1273f7c15ea9064c862bf1dbb7
matteofercia <m.fercia1@studenti.unica.it>
2022-05-03 10:24:47 +0200

    Creating a dictionary scraping the webiste to obtain a question-answer schema

--- scraping_web_faq.py
+++ scraping_web_faq.py
@@ -2,17 +2,6 @@ from pip._vendor import requests
 from bs4 import BeautifulSoup
 
 
-
-
-
-
-
-
-
-
-
-
-
 if __name__ == '__main__':
 
     URL = "https://www.stanzasemplice.com/en/faq"
@@ -22,9 +11,10 @@ if __name__ == '__main__':
     questions = soup.find_all("div", class_="domanda")
     answers = soup.find_all("div", itemprop="text")
 
+    #creatig a dictionary where every key is the question and every answer is the value
     diz_qa = {}
     for i in range(len(questions)):
-        diz_qa[questions[i]] = answers[i]
+        diz_qa[questions[i].text.strip().replace("\n"," ")] = answers[i].text.strip().replace("\n","").replace("\xa0"," ").replace("\"","").replace("\r"," ")
     print(diz_qa)
 
 
